346ec6be315cc7cf4c28c87856a1bebceee048e7
Sam <sdbernhardt@gmail.com>
2025-10-13 10:27:00 -0400

    Add shadcn; Scaffold out layout

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    allowedHosts: [
-      "29c1-2600-4040-7ed9-ea00-7dcb-45aa-c184-dd0.ngrok-free.app",
-    ],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-  plugins: [
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
-    react(),
-    tailwindcss(),
-  ],
 });
