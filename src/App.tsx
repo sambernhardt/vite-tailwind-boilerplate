ed086a622c5f8be1ae6638e3030c3ddc99241862
Sam <sdbernhardt@gmail.com>
2025-11-22 22:58:11 -0500

    Clean up initial theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <>
+    <div className="flex h-screen w-screen items-center justify-center">
       <h1>Vite + React + Tailwind</h1>
-    </>
+    </div>
   );
 }
 
